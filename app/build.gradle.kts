1d079f2a115ddee3ffd18b474d3ce82c7952e9c5
John Carl Dieparene <jcdieparene23@gmail.com>
2025-10-28 04:11:00 +0800

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,6 @@ android {
 
 dependencies {
 
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
 
 
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
